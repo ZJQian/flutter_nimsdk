347fbc60a49761ca04e0d3f6553370e86029625e
zjq <2326274522@qq.com>
2019-11-19 16:27:57 +0800

    新增文件

---
+++ lib/src/nim_history_message_search_option.dart
@@ -0,0 +1,44 @@
+import 'enum.dart';
+import 'nim_message_model.dart';
+
+
+class NIMHistoryMessageSearchOption {
+  int startTime;
+  int limit;
+  int endTime;
+  bool nimSync;
+  List<int> messageTypes;
+  NIMMessageSearchOrder order;
+  NIMMessage currentMessage;
+
+  NIMHistoryMessageSearchOption(
+      {this.startTime,
+      this.limit,
+      this.endTime,
+      this.nimSync,
+      this.messageTypes,
+      this.order,
+      this.currentMessage});
+
+  NIMHistoryMessageSearchOption.fromJson(Map<String, dynamic> json) {
+    startTime = json['startTime'];
+    limit = json['limit'];
+    endTime = json['endTime'];
+    nimSync = json['sync'];
+    messageTypes = json['messageTypes'].cast<int>();
+    order = json['order'];
+    currentMessage = json['currentMessage'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['startTime'] = this.startTime;
+    data['limit'] = this.limit;
+    data['endTime'] = this.endTime;
+    data['sync'] = this.nimSync;
+    data['messageTypes'] = this.messageTypes;
+    data['order'] = this.order;
+    data['currentMessage'] = this.currentMessage;
+    return data;
+  }
+}
